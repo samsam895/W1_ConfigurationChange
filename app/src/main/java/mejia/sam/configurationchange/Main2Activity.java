27e812ba51d5927fd56e3e84b9900180adb958f0
nicholaspark09 <mobappsam@gmail.com>
2017-02-07 20:04:32 -0500

    First commit

---
+++ app/src/main/java/mejia/sam/configurationchange/Main2Activity.java
@@ -0,0 +1,21 @@
+package mejia.sam.configurationchange;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        tv = (TextView) findViewById(R.id.tvmn2);
+
+        Bundle extras = getIntent().getExtras();
+        if(extras !=null) {
+            String value = extras.getString("MIO");
+            tv.setText(value);
+        }
+    }
+}
---
+++ app/src/main/java/mejia/sam/configurationchange/MainActivity.java
@@ -0,0 +1,48 @@
+package mejia.sam.configurationchange;
+
+import android.content.Intent;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tv;
+    EditText et;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = ((TextView) findViewById(R.id.tv));
+        et = ((EditText) findViewById(R.id.et));
+        Button btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Toast.makeText(MainActivity.this, et.getText().toString(), Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(MainActivity.this, Main2Activity.class);
+                String s = et.getText().toString();
+                i.putExtra("MIO",s);
+                startActivity(i);
+            }
+        });
+    }
+
+    public void btn(View view) {
+    }
+
+
+//    public void onDo (View view){
+//        tv.setText(et.getText().toString());
+//    }
+
+//    @Override
+//    protected void onSaveInstanceState(Bundle outState) {
+//        super.onSaveInstanceState(outState);
+//    }
+}
